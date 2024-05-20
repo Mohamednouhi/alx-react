b90cd88d5555fb63bd5b758f4bd3a6794d9cef20
mohamednouhi <mohamednoouuhi@gmail.com>
2024-05-20 17:31:24 +0100

    task1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="start">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('#start').click(debounce(updateCounter, 250));
